04dd1d3bc2649e65ebef8a83041a9569cb1371d0
Clyff Millet <clyffm@gmail.com>
2018-06-11 16:53:18 -0400

    firebase added to project

---
+++ merkleProto/Model/Thing.swift
@@ -0,0 +1,17 @@
+//
+//  Thing.swift
+//  merkleProto
+//
+//  Created by Clyfford Millet on 6/9/18.
+//  Copyright © 2018 Clyff Millet. All rights reserved.
+//
+
+import Foundation
+
+class Thing {
+    
+    var openToBuy: Bool!
+    var bought: Bool!
+    var imageMeta: String!
+    var cost: Int!
+}
