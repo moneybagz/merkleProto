bf405794c27359194f54a40c7d3c42e6966c7085
Clyff Millet <clyffm@gmail.com>
2018-06-17 23:56:26 -0400

    email and password saving

--- merkleProto/Controller/LoginVC.swift
+++ merkleProto/Controller/LoginVC.swift
@@ -11,21 +11,45 @@ import UIKit
 class LoginVC: UIViewController {
     
     
-    @IBOutlet var emailField: UITextField!
-    
+    @IBOutlet var emailField: UITextField!    
     @IBOutlet var passwordField: UITextField!
+    @IBOutlet var rememberMeBtn: UIButton!
+    
+    
+    var rememberMe: Bool = false
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Automatic text fill
+        if UserDefaults.standard.string(forKey: "email") != nil && UserDefaults.standard.string(forKey: "password") != nil {
+            emailField.text = UserDefaults.standard.string(forKey: "email")
+            passwordField.text = UserDefaults.standard.string(forKey: "password")
+        }
     }
-
+    
+    @IBAction func rememberMeBtnPressed(_ sender: Any) {
+        if rememberMe == false {
+            rememberMe = true
+            rememberMeBtn.backgroundColor = UIColor.red
+        } else {
+            rememberMe = false
+            rememberMeBtn.backgroundColor = UIColor.cyan
+        }
+    }
+    
     @IBAction func SignInBtnPressed(_ sender: Any) {
         if emailField.text != nil && passwordField.text != nil {
             AuthService.instance.loginUser(withEmail: emailField.text!, andPassword: passwordField.text!) { (success, loginError) in
                 if success {
+                    
+                    // Saving email and password
+                    if self.rememberMe == true {
+                        UserDefaults.standard.set(self.emailField.text, forKey: "email")
+                        UserDefaults.standard.set(self.passwordField.text, forKey: "password")
+                    }
+                    
                     self.dismiss(animated: true, completion: nil)
                 } else {
                     print(String(describing: loginError?.localizedDescription))
@@ -34,6 +58,13 @@ class LoginVC: UIViewController {
                 AuthService.instance.registerUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, userCreationComplete: { (success, registrationError) in
                     if success {
                         AuthService.instance.loginUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, loginComplete: { (success, registrationError) in
+                            
+                            // Saving email and password
+                            if self.rememberMe == true {
+                                UserDefaults.standard.set(self.emailField.text, forKey: "email")
+                                UserDefaults.standard.set(self.passwordField.text, forKey: "password")
+                            }
+                            
                             self.dismiss(animated: true, completion: nil)
                         })
                     } else {
